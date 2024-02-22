c82a5aaff097a8199db26ff2fd1938d846a79633
Jeremy LORTHIOIR <jeremy.lorthioir24@gmail.com>
2024-02-22 08:10:00 +0100

    fix: set java project in root directory

--- src/main/java/com/house/housemanager/controller/TaskController.java
+++ src/main/java/com/house/housemanager/controller/TaskController.java
@@ -2,15 +2,12 @@ package com.house.housemanager.controller;
 
 import java.util.UUID;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.house.housemanager.exceptions.NotFoundException;
 import com.house.housemanager.model.Task;
